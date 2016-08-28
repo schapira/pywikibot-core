f52939a3ccec103a977bfb2ad2e15dca32d21ec0
xqt <info@gno.de>
2016-08-28 11:59:09 +0200

    [bugfix] Update lagpattern
    Backport I081fe872c437177 from core
    
    Bug: T144023
    Change-Id: I9588d79933a59b854afc00730f8f4be5f3fceed7

--- pywikibot/data/api.py
+++ pywikibot/data/api.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8  -*-
 """Interface to Mediawiki's api.php."""
 #
-# (C) Pywikibot team, 2007-2015
+# (C) Pywikibot team, 2007-2016
 #
 # Distributed under the terms of the MIT license.
 #
@@ -85,7 +85,8 @@ else:
 
 _logger = "data.api"
 
-lagpattern = re.compile(r"Waiting for [\d.]+: (?P<lag>\d+) seconds? lagged")
+lagpattern = re.compile(
+    r'Waiting for [\w. ]+: (?P<lag>\d+)(?:\.\d+)? seconds? lagged')
 
 
 class APIError(Error):
